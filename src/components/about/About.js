c2f8c1db8ddd51ad5f8a16c090a7dad0969a9d64
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-12-05 17:32:27 +0100

    WIP image grid on about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,41 @@ function About() {
         <>
             <div className="about" id="about">
                 <div className="left">
-
+                    <div className="row">
+                        <div className="column">
+                            <img src="assets/grid/goutham-krishna.jpg" alt=""
+                            />
+                            <img src="assets/grid/kattis_lejonlid.jpg" alt=""/>
+                            <img src="assets/grid/swans.jpg" alt=""/>
+                            <img src="assets/grid/uwc_event.jpg" alt=""/>
+                            <img src="assets/grid/goutham-krishna.jpg" alt=""/>
+                        </div>
+                        <div className="column">
+                            <img src="assets/grid/kattis_omega.jpg" alt=""/>
+                            <img src="assets/grid/cookie.jpg" alt=""/>
+                            <img src="assets/grid/lukasz-szmigiel.jpg" alt=""/>
+                            <img src="assets/grid/marvin-meyer.jpg" alt=""/>
+                            <img src="assets/grid/swans.jpg" alt=""/>
+                            <img src="assets/grid/kattis_omega.jpg" alt=""/>
+                        </div>
+                        <div className="column">
+                            <img src="assets/grid/alterlego.jpg" alt=""/>
+                            <img src="assets/grid/cookie.jpg" alt=""/>
+                            <img src="assets/grid/goutham-krishna.jpg" alt=""/>
+                            <img src="assets/grid/kattis_lejonlid.jpg" alt=""/>
+                            <img src="assets/grid/swans.jpg" alt=""/>
+                            <img src="assets/grid/uwc_event.jpg" alt=""/>
+                            <img src="assets/grid/goutham-krishna.jpg" alt=""/>
+                        </div>
+                        <div className="column">
+                            <img src="assets/grid/kattis_omega.jpg" alt=""/>
+                            <img src="assets/grid/cookie.jpg" alt=""/>
+                            <img src="assets/grid/lukasz-szmigiel.jpg" alt=""/>
+                            <img src="assets/grid/marvin-meyer.jpg" alt=""/>
+                            <img src="assets/grid/swans.jpg" alt=""/>
+                            <img src="assets/grid/kattis_omega.jpg" alt=""/>
+                        </div>
+                    </div>
                 </div>
                 <div className="right">
                     <div className="wrapper">
