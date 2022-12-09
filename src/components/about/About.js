196da3ae2a312d2fe372db7c0aab7ec62ba89395
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-12-09 17:12:42 +0100

    Content fixing

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -17,7 +17,19 @@ function About() {
                 <div className="right">
                     <div className="wrapper">
                         <h1>About</h1>
-                        <p>This is a section about me</p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda beatae
+                            consectetur consequatur dignissimos excepturi facilis, illo laborum, magnam nulla obcaecati
+                            optio perspiciatis praesentium quaerat sed velit voluptas voluptate voluptatibus.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda beatae
+                            consectetur consequatur dignissimos excepturi facilis, illo laborum, magnam nulla obcaecati
+                            optio perspiciatis praesentium quaerat sed velit voluptas voluptate voluptatibus. Lorem
+                            ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda beatae
+                            consectetur consequatur dignissimos excepturi facilis, illo laborum, magnam nulla obcaecati
+                            optio perspiciatis praesentium quaerat sed velit voluptas voluptate voluptatibus.
+                        </p>
                     </div>
                 </div>
             </div>
