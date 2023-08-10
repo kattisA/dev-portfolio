bef9ef26e0139062e9d72bf91ca479bc951c9af0
Katarina Lejonlid <kattis.a@gmail.com>
2023-08-10 17:20:02 +0200

    #8: pages changes

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -14,6 +14,20 @@ function Pages() {
             link:"https://kattisa.github.io/marvel-search/"
         }
     ]
+
+    const arrowBox = () => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 width="16"
+                 height="16"
+                 fill="#eaddec"
+                 className="bi bi-arrow-up-right-square"
+                 viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+            </svg>
+        );
+    }
     return (
         <div className="pages" id="pages">
             <h2>My pages</h2>
@@ -24,8 +38,8 @@ function Pages() {
                     <div className="column" key={index}>
                         <div className="basicCard pageCard">
                             <h3>{page.title}</h3>
-                            <p>{page.description}</p>
-                            <a href={page.link} className="button">Visit page</a>
+                            <div className="content">{page.description}</div>
+                            <a href={page.link} className="button">  Visit page {arrowBox()}</a>
                         </div>
                     </div>
                 ))}
